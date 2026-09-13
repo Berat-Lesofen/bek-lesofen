0b4c233528a9ad6ee7e7fcf1e29e82aaf3831e43
Berat-Lesofen <172084414+Berat-Lesofen@users.noreply.github.com>
2026-09-13 08:40:17 +0300

    fix(branding): finalize horizontal navbar logo

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const header = document.querySelector('.site-header');
 
 function setCommonContent() {
   const isArticle = body.dataset.page === 'article';
-  const brandLogoPath = isArticle ? '../assets/images/bek-lesofen-navbar.png' : 'assets/images/bek-lesofen-navbar.png';
+  const brandLogoPath = isArticle ? '../assets/images/bek-lesofen-navbar2.png' : 'assets/images/bek-lesofen-navbar2.png';
   const footerLogoPath = isArticle ? '../assets/images/yenilogo.png' : 'assets/images/yenilogo.png';
   const logoEl = document.querySelector('#logo');
   if (logoEl) {
-    logoEl.innerHTML = `<img src="${brandLogoPath}" alt="${siteContent.siteName}" class="logo-navbar-img" width="189" height="52" />`;
+    logoEl.innerHTML = `<img src="${brandLogoPath}" alt="${siteContent.siteName}" class="logo-navbar-img" width="204" height="68" />`;
   }
   const footerLogoEl = document.querySelector('#footer-logo');
   if (footerLogoEl) {
